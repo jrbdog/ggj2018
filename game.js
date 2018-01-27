d9d9a7873c79983e6e1239456ea724d471f9b9a0
Lucas Schmidt <lschmidt@uccs.edu>
2018-01-27 11:41:20 -0700

    pick random word, encode with basic substitution cipher

--- game.js
+++ game.js
@@ -1,33 +1,44 @@
+const chalk = require('chalk');
+// const clui = require('clui');
 const readlineSync = require('readline-sync');
 const fs = require("fs");
+
+const morse = require("morse");
+const MonoAlphabeticCipher = require('text-ciphers').MonoAlphabeticCipher;
+const monoalphabeticCipher = new MonoAlphabeticCipher({
+    substitution: MonoAlphabeticCipher.createKeyByShift(-5)
+});
+
+const utility = require('./utility')
 
 // load puzzles
 var puzzleFile = fs.readFileSync("puzzles.json");
 var puzzles = JSON.parse(puzzleFile);
 
-console.log("debug: loaded "+puzzles.length+" puzzles")
+// load words
+var words = fs.readFileSync('words_alpha.txt').toString().split("\n");
+console.log("Loaded "+ words.length + " words");
 
-var puzzleNum = getRandomInt(0, puzzles.length)
+//console.log("debug: loaded "+puzzles.length+" puzzles")
+//var puzzleNum = utility.getRandomInt(0, puzzles.length)
 
-console.log("===ENCRYPTED MESSAGE===");
-console.log(puzzles[puzzleNum].message);
+var randomWord = words[utility.getRandomInt(0, words.length)];
+// console.log("debug: word is [" + randomWord + "]");
+encodedMsg = monoalphabeticCipher.encipher(randomWord);
+//encodedMsg = morse.encode(randomWord);
+
+console.log(chalk.cyan("===INCOMING TRANSMISSION==="));
+console.log(encodedMsg);
 
 var isAnswered = false;
 
 while (!isAnswered) {
     var userAnswer = readlineSync.question('Enter your answer: ');
-    if (userAnswer == puzzles[puzzleNum].answer) {
-        console.log("CORRECT");
+    if (userAnswer == randomWord) {
+        console.log(chalk.green("CORRECT"));
         isAnswered = true;
     }
     else {
-        console.log("INCORRECT");
+        console.log(chalk.red("INCORRECT"));
     }
-}
-
-// utility functions
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
 }
