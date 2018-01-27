1aeb987e96443112578a80dcbc437c2a09da9a67
Lucas Schmidt <lschmidt@uccs.edu>
2018-01-27 16:09:48 -0700

    Encryption routine for red path

--- encryption.js
+++ encryption.js
@@ -13,7 +13,36 @@ const monoalphabeticCipher2 = new MonoAlphabeticCipher({
 
 const utility = require("./utility")
 
-const numberOfEncryptionMethods = 3;
+const numberOfEncryptionMethods = 1;
+
+const letterToNumberRed = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26
+};
 
 exports.randomEncryption = function(messageToEncode) {
     let encNum = utility.getRandomInt(1, numberOfEncryptionMethods + 1);
@@ -22,7 +51,54 @@ exports.randomEncryption = function(messageToEncode) {
     let encodedMessage = '';
     switch (encNum) {
         case 1:
-            encodedMessage = chalk.red(monoalphabeticCipher1.encipher(messageToEncode));
+            let msg = messageToEncode;
+            Object.keys(letterToNumberRed).forEach(function(key) {
+                let value = letterToNumberRed[key];
+                msg = msg.replace(new RegExp(key, 'g'), value + " ");
+            }, this);
+
+            // check for all even, if so multiply by 2
+            let numberArray = msg.split(" ");
+            let allEven = true;
+            numberArray.forEach(function(n) {
+                if (utility.isOdd(n)) { allEven = false }
+            }, this);
+
+            if (allEven) {
+                console.log('debug: multiplying all by 2');
+
+                let origNumArray = msg.split(" ");
+                let newNumArray = [];
+                
+                console.log(origNumArray);
+                origNumArray.forEach(function(n) {
+                    if(n > 0) {
+                        newNumArray.push(n*2);
+                    }
+                }, this);
+                console.log(newNumArray);
+                msg = newNumArray.join(" ");
+            }
+
+            // chance of appending random repeating number
+            if (Math.floor(Math.random()*2) == 1) {
+                console.log('debug: inserting repeating number')
+                let repeatingNum = utility.getRandomInt(1, 10);
+                let origNumArray = msg.split(" ");
+                // console.log(origNumArray);
+
+                let newNumArray = [];
+                origNumArray.forEach(function(n) {
+                    if(n > 0) {
+                        newNumArray.push(n);
+                        newNumArray.push(repeatingNum);
+                    }
+                }, this);
+                // console.log(newNumArray);
+                msg = newNumArray.join(" ");
+            }
+
+            encodedMessage = chalk.red(msg);
             break;
 
         case 2:
@@ -31,6 +107,10 @@ exports.randomEncryption = function(messageToEncode) {
     
         case 3:
             encodedMessage = morse.encode(messageToEncode);
+            break;
+
+        case 4:
+            encodedMessage = chalk.blue(monoalphabeticCipher1.encipher(messageToEncode));
             break;
 
         default:
--- utility.js
+++ utility.js
@@ -3,3 +3,11 @@ exports.getRandomInt = function(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+exports.isEven = function(n) {
+    return n % 2 == 0;
+ }
+ 
+exports.isOdd = function(n) {
+    return Math.abs(n % 2) == 1;
+ }
