8eb13b655c74816c4edad3402209c562eb12d39d
Lucas Schmidt <lschmidt@uccs.edu>
2018-01-28 13:03:01 -0700

    Story CSS adjustments

--- story.js
+++ story.js
@@ -1,5 +1,8 @@
+// Text and/or HTML goes here. Array index starts at 0, so first item is intentionally blank.
+// Use backticks (`) for multi-line entries.
 export const story = [
     "",
-    "story step 1 <strong>here</strong><br>test",
-    "story step <b>2</b> here"
+    `story step 1 <strong>here</strong><br>
+    test`,
+    `story step <b>2</b> here`
 ]
